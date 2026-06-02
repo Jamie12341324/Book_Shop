ceb63f5012dee97be5ddfcdcc97e53901aa038e3
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-06-02 13:20:08 +0100

    Remove env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ.setdefault("DATABASE_URL","postgresql://neondb_owner:npg_emW80UYXuJRi@ep-mute-breeze-alnr2bha.c-3.eu-central-1.aws.neon.tech/bookshop")
-os.environ.setdefault("SECRET_KEY", "django-insecure-ym@1(5hqtqz^p)w)hn=22#v(hma=d90wm2axjz+w(e1v04*!*4")
-os.environ.setdefault("CLOUDINARY_URL", "cloudinary://951791114181516:aIjZXE3MTIM3LlwALQ4FDUYQ6Ho@dapj1ryeu")
-os.environ.setdefault("DEBUG_SWITCH","True")
