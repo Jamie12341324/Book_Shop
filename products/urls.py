9827922d5fc514076607e2a592c021e85272695e
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-06-08 09:20:22 +0100

    Webhook test

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('checkout/<int:product_id>/', views.create_checkout_session, name='create_checkout_session'),
     path('success/', views.payment_success, name='payment_success'),
     path('cancel/', views.payment_cancel, name='payment_cancel'),
+    path('webhook/', views.stripe_webhook, name='stripe_webhook'),
     
     # path('<int:product_id>/', views.product_detail, name='product_detail'),
     # path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
