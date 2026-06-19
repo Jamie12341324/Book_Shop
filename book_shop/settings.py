f9325214e683b3251d774e5e9b27953d76681fc6
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-06-19 08:48:51 +0100

    Stop connection refused error on SignUp by disabling email verification

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -35,6 +35,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
