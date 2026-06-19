bc990e5ff0156f54bee6e4ed1964ae2c2ec27014
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-06-19 18:51:50 +0100

    Add problem9

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# ACCOUNT_EMAIL_VERIFICATION from stackoverflow
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 ALLOWED_HOSTS = [
